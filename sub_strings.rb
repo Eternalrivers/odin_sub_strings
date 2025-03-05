e8ce10fa2f050cba01edd3e608acca45c51c2599
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-03-05 10:15:08 +0800

    fix: fix error caused by wrong parameter
    Using 'pry-byebug', found error in the method of using a wrong parameter

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,9 +1,19 @@
+require 'pry-byebug'
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+str = 'below'
+
 def sub_string (string, dictionary)
+  binding.pry
+
   arr = []
+  
   dictionary.each do |word|
-    if str.include?(word)
-      arr << wordend
+    if string.include?(word)
+      arr << word
     end
   end
   arr.tally
-  end
+end
+
+sub_string(str,dictionary)
